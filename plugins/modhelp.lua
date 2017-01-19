5b1fe714fe99183a44b3beb6ce6ce162462b2f0e
stuxnettel <armaneblis28@gmail.com>
2017-01-18 22:12:37 -0800

    Update modhelp.lua

--- plugins/modhelp.lua
+++ plugins/modhelp.lua
@@ -127,8 +127,8 @@ if msg.to.type == 'channel' and is_momod(msg) then
 
 ➰شما میتوانید از '/' یا '!' یا '#' برای اجرای دستورات استفاده کنید.
 ➖➖➖➖➖➖➖
-➰ ʝօìղ մʂ ìժ çհ :
-➰ @antispamandhack
+➰create by :
+➰@GOD_B0Y 
 ➖➖➖➖➖➖➖ ]]
 end
 end
